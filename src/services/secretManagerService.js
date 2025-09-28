3d819384073db3297d2638379c029ba4bff62318
brogeth <niamn3@gmail.com>
2025-09-28 14:23:41 +1000

    Bug fixes

--- src/services/secretManagerService.js
+++ src/services/secretManagerService.js
@@ -4,87 +4,55 @@ const region = process.env.AWS_REGION || "ap-southeast-2";
 const secretsManagerClient = new SecretsManagerClient({ region: region });
 
 let jwtSecret = null;
-let jwtSecretInitialised = false;
-let _resolveJwtSecretInitialised;
-
 let cognitoClientSecret = null;
-let cognitoClientSecretInitialised = false;
-let _resolveCognitoClientSecretInitialised;
-
-async function getJwtSecret() {
-    if (jwtSecretInitialised) {
-        return jwtSecret;
-    }
-
-    try {
-        const secret_name = "n11051337-A2-JWT";
-        const response = await secretsManagerClient.send(
-            new GetSecretValueCommand({
-                SecretId: secret_name
-            })
-        );
-
-        if (response.SecretString) {
-            const secrets = JSON.parse(response.SecretString);
-            jwtSecret = secrets.JWT_SECRET;
-            jwtSecretInitialised = true;
-            if (_resolveJwtSecretInitialised) _resolveJwtSecretInitialised();
-            return jwtSecret;
-        }
-    } catch (error) {
-        console.error("Error retrieving JWT secret from AWS Secrets Manager:", error);
-        process.exit(1);
-    }
-}
-
-async function getCognitoClientSecret() {
-    if (cognitoClientSecretInitialised) {
-        return cognitoClientSecret;
-    }
-
-    try {
-        const secret_name = "n11051337-A2-Cognito";
-        const response = await secretsManagerClient.send(
-            new GetSecretValueCommand({
-                SecretId: secret_name
-            })
-        );
-
-        if (response.SecretString) {
-            const secrets = JSON.parse(response.SecretString);
-            cognitoClientSecret = secrets.COGNITO_CLIENT_SECRET;
-            cognitoClientSecretInitialised = true;
-            if (_resolveCognitoClientSecretInitialised) _resolveCognitoClientSecretInitialised();
-            return cognitoClientSecret;
-        }
-    } catch (error) {
-        console.error("Error retrieving Cognito Client Secret from AWS Secrets Manager:", error);
-        process.exit(1);
-    }
-}
-
-const jwtInitialisedPromise = new Promise(resolve => {
-    _resolveJwtSecretInitialised = resolve;
-});
-
-const cognitoClientSecretInitialisedPromise = new Promise(resolve => {
-    _resolveCognitoClientSecretInitialised = resolve;
-});
-
-getJwtSecret();
-getCognitoClientSecret();
 
 module.exports = {
     getJwtSecret: async () => {
-        if (!jwtSecretInitialised) {
-            await jwtInitialisedPromise;
+        if (jwtSecret) {
+            return jwtSecret;
         }
-        return jwtSecret;
+
+        try {
+            const secret_name = "n11051337-A2-JWT";
+            const response = await secretsManagerClient.send(
+                new GetSecretValueCommand({
+                    SecretId: secret_name
+                })
+            );
+
+            if (response.SecretString) {
+                const secrets = JSON.parse(response.SecretString);
+                jwtSecret = secrets.JWT_SECRET;
+                return jwtSecret;
+            }
+        } catch (error) {
+            console.error("Error retrieving JWT secret from AWS Secrets Manager:", error);
+            process.exit(1);
+        }
+        return null;
     },
     getCognitoClientSecret: async () => {
-        if (!cognitoClientSecretInitialised) {
-            await cognitoClientSecretInitialisedPromise;
+        if (cognitoClientSecret) {
+            return cognitoClientSecret;
         }
-        return cognitoClientSecret;
+
+        try {
+            const secret_name = "n11051337-A2-Cognito";
+            const response = await secretsManagerClient.send(
+                new GetSecretValueCommand({
+                    SecretId: secret_name
+                })
+            );
+
+            if (response.SecretString) {
+                const secrets = JSON.parse(response.SecretString);
+                cognitoClientSecret = secrets.COGNITO_CLIENT_SECRET;
+                return cognitoClientSecret;
+            }
+        } catch (error) {
+            console.error("Error retrieving Cognito Client Secret from AWS Secrets Manager:", error);
+            process.exit(1);
+        }
+        return null;
     }
 };
