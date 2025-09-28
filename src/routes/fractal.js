3ea6f21e4366289cac486194e499d7aabec84fc6
brogeth <niamn3@gmail.com>
2025-09-28 20:02:31 +1000

    Caching stuff

--- src/routes/fractal.js
+++ src/routes/fractal.js
@@ -62,6 +62,25 @@ router.get('/fractal', verifyToken, async (req, res) => {
                 console.log(`DEBUG: /fractal - Adding existing fractal to gallery for user.`);
                 galleryId = await Gallery.addToGallery(req.user.id, row.id, row.hash);
                 console.log(`DEBUG: /fractal - Added existing fractal to gallery with ID: ${galleryId}`);
+                // Invalidate all possible cache keys for the user's gallery
+                const commonFilters = [{}, { colourScheme: 'viridis' }, { power: 2 }, { iterations: 100 }];
+                const commonSortBys = ['added_at', 'hash'];
+                const commonSortOrders = ['ASC', 'DESC'];
+                const commonLimits = [5, 10, 20];
+                const commonOffsets = [0, 5, 10];
+
+                for (const filter of commonFilters) {
+                    for (const sortBy of commonSortBys) {
+                        for (const sortOrder of commonSortOrders) {
+                            for (const limit of commonLimits) {
+                                for (const offset of commonOffsets) {
+                                    const userCacheKey = generateCacheKey(req.user.id, filter, sortBy, sortOrder, limit, offset);
+                                    await cacheService.del(userCacheKey);
+                                }
+                            }
+                        }
+                    }
+                }
             }
 
             const fractalUrl = await s3Service.getPresignedUrl(row.s3_key);
@@ -103,6 +122,25 @@ router.get('/fractal', verifyToken, async (req, res) => {
             console.log(`DEBUG: /fractal - Adding new fractal to gallery for user.`);
             const newGalleryId = await Gallery.addToGallery(req.user.id, result.id, hash);
             console.log(`DEBUG: /fractal - Added new fractal to gallery with ID: ${newGalleryId}`);
+            // Invalidate all possible cache keys for the user's gallery
+            const commonFilters = [{}, { colourScheme: 'viridis' }, { power: 2 }, { iterations: 100 }];
+            const commonSortBys = ['added_at', 'hash'];
+            const commonSortOrders = ['ASC', 'DESC'];
+            const commonLimits = [5, 10, 20];
+            const commonOffsets = [0, 5, 10];
+
+            for (const filter of commonFilters) {
+                for (const sortBy of commonSortBys) {
+                    for (const sortOrder of commonSortOrders) {
+                        for (const limit of commonLimits) {
+                            for (const offset of commonOffsets) {
+                                const userCacheKey = generateCacheKey(req.user.id, filter, sortBy, sortOrder, limit, offset);
+                                await cacheService.del(userCacheKey);
+                            }
+                        }
+                    }
+                }
+            }
 
             const fractalUrl = await s3Service.getPresignedUrl(s3Key);
             res.json({ hash, url: fractalUrl, galleryId: newGalleryId });
