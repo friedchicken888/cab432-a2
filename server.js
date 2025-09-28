b6e01062221c2f3e775c65a375afd91d3228af0e
brogeth <niamn3@gmail.com>
2025-09-28 14:16:43 +1000

    Refactored code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const { initialiseAuth } = require('./src/routes/auth');
+const { router: authRouter, verifyToken } = require('./src/routes/auth');
 const fractalRouter = require('./src/routes/fractal');
 const historyRouter = require('./src/routes/history');
 
@@ -12,13 +12,12 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/fractals', express.static('fractals'));
 
+app.use('/api/auth', authRouter);
+app.use('/api', fractalRouter);
+app.use('/api', historyRouter);
+
 (async () => {
   try {
-    const { router: authRouter, verifyToken } = await initialiseAuth();
-    app.use('/api/auth', authRouter);
-    app.use('/api', fractalRouter);
-    app.use('/api', historyRouter);
-
     await s3Service.ensureBucketAndTags();
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
